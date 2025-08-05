b4c756e912b7032eb5ba3108d927d5b2ea1d6d4c
pective <pective@proton.me>
2025-08-05 20:04:17 +0200

    Nav buttons look good now

--- src/index.js
+++ src/index.js
@@ -5,12 +5,17 @@ import createMenuPage from './menuPage.js';
 const navButtons = document.querySelectorAll('button');
 
 navButtons.forEach((button) => {
+    
     button.addEventListener("click", () => {
+        navButtons.forEach((btn) => {
+            btn.style.backgroundColor = "#a46b51ff";
+        })
         if (button.textContent === 'Home') {
             createHomePage();
         } else if (button.textContent === 'Menu') {
             createMenuPage();
         }
+        button.style.backgroundColor = "#c28e76ff";
     })
 })
 
